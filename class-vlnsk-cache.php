a05d595294e42e03fa587e63f3b2d7fdf0a01ac7
Dima Solovey <38838582+disolovei@users.noreply.github.com>
2019-09-22 17:49:34 +0300

    The skeleton of the class

--- class-vlnsk-cache.php
+++ class-vlnsk-cache.php
@@ -1,1 +1,33 @@
 
+<?php
+class VLNSK_Cache() {
+
+    protected $data = array();
+
+    public static function instance() {
+
+    }
+
+    private function __construct() {}
+
+    public function set( $key, $value, $group = '', $overrewrite = true ) {
+
+    }
+
+    public function get( $key, $group, $default_value = null ) {
+
+    }
+
+    public function update( $key, $value, $group = '' ) {
+
+    }
+
+    public function delete( $key, $group = '' ) {
+
+    }
+
+    public function isset( $key, $group = '' ) {
+
+    }
+
+}
